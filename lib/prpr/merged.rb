5604dad6d1dbccb5d78292433c707ae45788fca0
mzp <mzpppp@gmail.com>
2016-06-21 18:25:28 +0900

    bundle gem prpr-merged

---
+++ lib/prpr/merged.rb
@@ -0,0 +1,7 @@
+require "prpr/merged/version"
+
+module Prpr
+  module Merged
+    # Your code goes here...
+  end
+end
---
+++ lib/prpr/merged/version.rb
@@ -0,0 +1,5 @@
+module Prpr
+  module Merged
+    VERSION = "0.1.0"
+  end
+end
