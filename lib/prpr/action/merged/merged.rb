b0193dd9842fd0001e74bfd479542a6cbdf976f7
Daiki Hirakata <daiki@bigplants.net>
2018-11-01 13:40:58 +0900

    customize body.

--- lib/prpr/action/merged/merged.rb
+++ lib/prpr/action/merged/merged.rb
@@ -11,15 +11,36 @@ module Prpr
         private
 
         def message
-         Prpr::Publisher::Message.new(body: body, from: from, room: room)
+          Prpr::Publisher::Message.new(body: body, from: from, room: room)
         end
 
         def merged?
-          !!event.pull_request.merged_at
+          !!pull_request.merged_at
+        end
+
+        def head_ref
+          pull_request.head.ref
+        end
+
+        def base_ref
+          pull_request.base.ref
+        end
+
+        def customize_params
+          Hash[
+            html_url: pull_request.html_url,
+            title: pull_request.title,
+            head: head_ref,
+            base: base_ref
+          ]
+        end
+
+        def pull_request
+          event.pull_request
         end
 
         def body
-          env.format(:merged_body, event.pull_request)
+          env.format(:merged_body, customize_params)
         end
 
         def from
